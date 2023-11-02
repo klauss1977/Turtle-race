1dbdce4f7455295b0703f9c42d84d77d980966e0
klauss1977 <klauss1977@yahoo.com>
2023-11-02 12:54:10 +0200

    add finish line and bet

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from turtle import Turtle, Screen
 import random
-is_race_on=False
+
+is_race_on = False
 screen = Screen()
-screen.setup(width=500, height=400)
+screen.setup(width=900, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-colors=['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-y_positions = [-70, -40, -10, 20, 50, 80 ]
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+y_positions = [-70, -40, -10, 20, 50, 80]
 all_turtles = []
+finish = Turtle()
+finish.speed('fastest')
+finish.hideturtle()
+finish.pensize(5)
+
+
+def draw_finish_line():
+    finish.penup()
+    finish.goto(230, -250)
+    for _ in range(15):
+        finish.pendown()
+        finish.goto(230, finish.ycor() + 20)
+        finish.penup()
+        finish.goto(230, finish.ycor() + 20)
+
 
 for turtle_index in range(0, 6):
     new_turtle = Turtle(shape="turtle")
@@ -17,30 +33,25 @@ for turtle_index in range(0, 6):
 
 if user_bet:
     is_race_on = True
+    draw_finish_line()
+
+for turtle in all_turtles:
+    if user_bet == turtle.pencolor():
+        turtle.write("BET")
 
 while is_race_on:
     for turtle in all_turtles:
-        if turtle.xcor()>=225:
-            screen.title(f"Winner is {turtle.pencolor()} turtle")
-            is_race_on=False
-            if user_bet==turtle.pencolor():
-                print(f"You won! The {turtle.pencolor()} turtle is the winner")
+
+        if turtle.xcor() >= 225:
+            is_race_on = False
+            if user_bet == turtle.pencolor():
+
+                turtle.write("You won!", align="center", font=("Cooper Black", 25, "italic"))
 
             else:
-                print(f"You lost! The {turtle.pencolor()} turtle is the winner")
-        random_distance=random.randint(0,9)
+                turtle.write("You lost!", align="center", font=("Cooper Black", 25, "italic"))
+
+        random_distance = random.randint(0, 9)
         turtle.forward(random_distance)
-
-
-
-
-
-
-
-
-
-
-
-
 
 screen.exitonclick()
